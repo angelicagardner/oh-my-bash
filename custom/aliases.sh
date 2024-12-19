aeb47870b3644771ef499920596e025bd4866d3d
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2024-12-19 18:36:09 +0100

    feat: override git branch alias

--- custom/aliases.sh
+++ custom/aliases.sh
@@ -1,2 +1,6 @@
+# Override
+alias gb="git --no-pager branch"
+
+# Additional
 alias k='kubectl'
 alias kubeconfig-service-cluster='export KUBECONFIG=/path/to/kubeconfig; kubectl config set-context --current --namespace=ns'
