9d27c06d0a82125a97f9319edd48b998d9e1671c
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2024-12-20 16:29:38 +0100

    feat: add bash customizations

--- custom/variables.sh
+++ custom/variables.sh
@@ -1,17 +1,22 @@
 # tokens
 
+# Pip
+export PATH="$HOME/.local/bin:$PATH"
+# Go binary path
+export PATH=$PATH:/usr/local/go/bin
+#Set GOPATH, the workspace for Go projects
+export GOPATH=$HOME/go
+# Add GOPATH/bin path
+export PATH=$PATH:$GOPATH/bin
+
 # Increase Bash history size
 HISTSIZE=10000000
 HISTFILESIZE=10000000
-
 # Ignore specific commands from history
 HISTIGNORE="ls:cd:pwd:exit:*ls *:*cd *:*pwd *:*exit *"
-
 # Prevent duplicate entries and ignore commands starting with a space
 HISTCONTROL=ignoredups:erasedups
-
 # Append to the history file, don't overwrite it
 shopt -s histappend
-
 # Include timestamps in history
 export HISTTIMEFORMAT="%F %T "
