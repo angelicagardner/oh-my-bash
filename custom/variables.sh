ed1db28496439bb7ccb6d9a3beb3364c3851c35d
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2024-12-20 13:32:03 +0100

    feat: add history settings

--- custom/variables.sh
+++ custom/variables.sh
@@ -1,1 +1,17 @@
 # tokens
+
+# Increase Bash history size
+HISTSIZE=10000000
+HISTFILESIZE=10000000
+
+# Ignore specific commands from history
+HISTIGNORE="ls:cd:pwd:exit:*ls *:*cd *:*pwd *:*exit *"
+
+# Prevent duplicate entries and ignore commands starting with a space
+HISTCONTROL=ignoredups:erasedups
+
+# Append to the history file, don't overwrite it
+shopt -s histappend
+
+# Include timestamps in history
+export HISTTIMEFORMAT="%F %T "
