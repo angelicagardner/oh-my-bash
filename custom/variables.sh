f43ea7082a0ebe1ff361c654efaff8c99d42ce29
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2024-12-20 16:44:17 +0100

    feat: improve history duplicates ignore

--- custom/variables.sh
+++ custom/variables.sh
@@ -14,8 +14,8 @@ HISTSIZE=10000000
 HISTFILESIZE=10000000
 # Ignore specific commands from history
 HISTIGNORE="ls:cd:pwd:exit:*ls *:*cd *:*pwd *:*exit *"
-# Prevent duplicate entries and ignore commands starting with a space
-HISTCONTROL=ignoredups:erasedups
+# Prevent duplicates and space-prefixed commands (for privacy), along with erasing all duplicates before adding new ones
+HISTCONTROL=ignoreboth:erasedups
 # Append to the history file, don't overwrite it
 shopt -s histappend
 # Include timestamps in history
